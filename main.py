0ac196a5438b56b86c4bbc2f096fcae7c7bb7b9e
weightedEights <poormanjosh@gmail.com>
2017-05-04 13:38:48 -0200

    update main

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from datetime import datetime
+
+def main():
+    printHeader()
+  
+  
+def printHeader():
+    print("---------------------------------")
+    print("      matplotlib Tutorial")
+    print("   " + str(datetime.now()))
+    print("---------------------------------")
+
+
+if __name__ == '__main__':
+    main()
